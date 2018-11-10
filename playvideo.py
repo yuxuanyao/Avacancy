d0376ac5de4a7244039a0c14d0fabdff6356cbac
Yuxuan Yao <yuxuanyyx@gmail.com>
2018-11-10 16:49:44 -0500

    added distance calculations

--- playvideo.py
+++ playvideo.py
@@ -8,22 +8,48 @@ while(cap.isOpened()):
 
 	#gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-	cv2.circle(frame, (293, 96), 5, (0, 0, 255), -1)
-	cv2.circle(frame, (490, 100), 5, (0, 0, 255), -1)
-	cv2.circle(frame, (30, 310), 5, (0, 0, 255), -1)
-	cv2.circle(frame, (560, 330), 5, (0, 0, 255), -1)
+	# top left
+	tl = (293, 96)
+	# top right
+	tr = (490, 100)
+	#bottom left
+	bl = (30, 310)
+	#bottom right
+	br = (560, 330)
 
-	cv2.circle(frame, (0, 0), 5, (0, 255, 0), -1)
-	cv2.circle(frame, (600, 0), 5, (0, 255, 0), -1)
-	cv2.circle(frame, (0, 400), 5, (0, 255, 0), -1)
-	cv2.circle(frame, (600, 400), 5, (0, 255, 0), -1)
+	# max width
+	widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+	widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+	maxWidth = max(int(widthA), int(widthB))
 
-	pts1 = np.float32([[293, 96], [490, 100], [30, 310], [560, 330]])
-	pts2 = np.float32([[0, 0], [600, 0], [0, 400], [600, 400]])
+	# max height
+	heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+	heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+	maxHeight = max(int(heightA), int(heightB))
+
+	t1 = (0, 0)
+	t2 = (maxWidth, 0)
+	t3 = (0, maxHeight)
+	t4 = (maxWidth, maxHeight)
+
+	'''
+	cv2.circle(frame, tl, 5, (0, 0, 255), -1)
+	cv2.circle(frame, tr, 5, (0, 0, 255), -1)
+	cv2.circle(frame, bl, 5, (0, 0, 255), -1)
+	cv2.circle(frame, br, 5, (0, 0, 255), -1)
+
+	cv2.circle(frame, t1, 5, (0, 255, 0), -1)
+	cv2.circle(frame, t2, 5, (0, 255, 0), -1)
+	cv2.circle(frame, t3, 5, (0, 255, 0), -1)
+	cv2.circle(frame, t4, 5, (0, 255, 0), -1)
+	'''
+
+	pts1 = np.float32([list(tl), list(tr), list(bl), list(br)])
+	pts2 = np.float32([list(t1), list(t2), list(t3), list(t4)])
 	
 	matrix = cv2.getPerspectiveTransform(pts1, pts2)
 
-	result = cv2.warpPerspective(frame, matrix, (600, 400))
+	result = cv2.warpPerspective(frame, matrix, (maxWidth, maxHeight))
 
 	cv2.imshow('result',result)
 	cv2.imshow('frame',frame)
@@ -32,4 +58,10 @@ while(cap.isOpened()):
 
 #while(True):
 cap.release()
-	#cv2.destroyAllWindows()
+cv2.destroyAllWindows()
+
+
+
+
+
+
