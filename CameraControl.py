0bbd00db65ddf64f57a0c83a9b0929b567c8019f
Jimmy <j35qi@uwaterloo.ca>
2018-11-10 14:45:48 -0500

    camera screenshot

--- CameraControl.py
+++ CameraControl.py
@@ -3,30 +3,24 @@ import cv2
 camera_port = 0
 ramp_frames = 30
 
-# cv2.namedWindow("preview")
+cv2.namedWindow("preview")
 vc = cv2.VideoCapture(camera_port)
 
-def get_image():
-    retval, im = vc.read()
-    return im
+# camera_capture = get_image()
+# file = "./screenshots/1.png"
+# cv2.imwrite(file, camera_capture)
 
-for i in range(ramp_frames):
-    get_image()
+if vc.isOpened(): # try to get the first frame
+    rval, frame = vc.read()
+else:
+    rval = False
 
-camera_capture = get_image()
-file = "./screenshots/1.png"
-cv2.imwrite(file, camera_capture)
+while rval:
+    cv2.imshow("preview", frame)
+    rval, frame = vc.read()
+    key = cv2.waitKey(1)
+    if key == 27: # exit on ESC
+        break
 
 del(vc)
-# if vc.isOpened(): # try to get the first frame
-#     rval, frame = vc.read()
-# else:
-#     rval = False
-
-# while rval:
-#     cv2.imshow("preview", frame)
-#     rval, frame = vc.read()
-#     key = cv2.waitKey(20)
-#     if key == 27: # exit on ESC
-#         break
-# cv2.destroyWindow("preview")
+cv2.destroyWindow("preview")
