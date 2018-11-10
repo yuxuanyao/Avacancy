d00814a3d4ca7c7d55e6c6f1da83503d3edda2da
Jimmy <j35qi@uwaterloo.ca>
2018-11-10 13:52:08 -0500

    add camera capture

---
+++ CameraControl.py
@@ -0,0 +1,19 @@
+import cv2
+
+camera_port = 1
+
+cv2.namedWindow("preview")
+vc = cv2.VideoCapture(camera_port)
+
+if vc.isOpened(): # try to get the first frame
+    rval, frame = vc.read()
+else:
+    rval = False
+
+while rval:
+    cv2.imshow("preview", frame)
+    rval, frame = vc.read()
+    key = cv2.waitKey(20)
+    if key == 27: # exit on ESC
+        break
+cv2.destroyWindow("preview")
